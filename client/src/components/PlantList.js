1866bcabb34de7866761e4e1adcba6734fbebfe0
djviodes <djviodes26@gmail.com>
2020-10-30 08:24:27 -0700

    Having issues with a weird error

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { response } from "express";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  componentDidMount() {
+    this.fetchPlants();
+  }
+
+  fetchPlants = () => {
+    axios.get(`http://localhost:3333/plants`)
+      .then(response => {
+        console.log(response)
+        this.setState({
+          plants: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
