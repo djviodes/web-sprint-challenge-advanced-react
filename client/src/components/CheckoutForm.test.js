7167b382224e65e3254b01805e19eed4bdf9adb9
djviodes <djviodes26@gmail.com>
2020-10-30 10:03:02 -0700

    Got all tests written and passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -13,4 +13,27 @@ test("form header renders", () => {
     expect(header).toHaveTextContent(/checkout form/i);
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+
+    fireEvent.change(firstNameInput, { target:{ value: 'David', name: 'firstName'}});
+    fireEvent.change(lastNameInput, { target:{ value: 'Viodes', name: 'lastName'}});
+    fireEvent.change(addressInput, { target:{ value: '7654 Numbers Way', name: 'address'}});
+    fireEvent.change(cityInput, { target:{ value: 'Numbers City', name: 'city'}});
+    fireEvent.change(stateInput, { target:{ value: 'NY', name: 'state'}});
+    fireEvent.change(zipInput, { target:{ value: '76543', name: 'zip'}});
+
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    await waitFor(() => {
+        expect(screen.getByTestId(/successmessage/i)).toBeInTheDocument();
+    })
+});
